ca6d777ee9697c64fd9f1c6579c910822e9db028
Rick Leinecker <rick@leinecker.com>
2024-06-26 23:48:09 -0400

    Initial Commit

--- frontend/src/components/CardUI.js
+++ frontend/src/components/CardUI.js
@@ -35,7 +35,7 @@ function CardUI() {
 
         try {
             // const response = await fetch('http://localhost:5000/api/addcard',
-            const response = fetch(buildPath('api/addcard'),
+            const response = await fetch(buildPath('api/addcard'),
                 { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
 
             var txt = await response.text();
